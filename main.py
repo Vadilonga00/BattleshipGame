28b5e59735906c41d17246f706f94fefbf7478a1
Massimo-Capurro-Llado <massimocapurrollado@gmail.com>
2022-02-09 10:09:58 +0100

    Added first parser

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
-#variables
-row_size = 10  # number of rows
-col_size = 10 # number of columns
-max_turns=60
-#initialize a board
+import parser
+
+args = parser.initialize_parser()
+
+# Variables
+row_size = args.rows
+col_size = args.columns
+max_turns = args.turns
+
+# Initialize the board
 ship_list = []
 board = [[0] * col_size for x in range(row_size)]
-#board_display = [["O"] * col_size for x in range(row_size)] in case of string
+
+
+# board_display = [["O"] * col_size for x in range(row_size)] # for strings case
+
 
 def print_board(board_array):
-  print("\n  " + " ".join(str(x) for x in range(1, col_size + 1)))
-  for r in range(row_size):
-    print(str(r + 1) + " " + " ".join(str(c) for c in board_array[r]))
-  print()
-
+    print("\n  " + " ".join(str(x) for x in range(1, col_size + 1)))
+    for r in range(row_size):
+        print(str(r + 1) + " " + " ".join(str(c) for c in board_array[r]))
+    print()
 
 
 for turn in range(max_turns):
-  print_board(board)
-  print("Turn:", turn + 1, "of", max_turns)
-  print("Ships left:", len(ship_list))
-  print()
+    print_board(board)
+    print("Turn:", turn + 1, "of", max_turns)
+    print("Ships left:", len(ship_list))
 
-  input("guess_row:\n")
+    row_guess = int(input("guess_row:\n"))
+    if row_guess > row_size:
+        print(f'That is not in the boundaries of the ocean! Try a coordinate between 1 and {row_size}')
 
-  input("guess_colum:\n")
-
+    col_guess = int(input("guess_colum:\n"))
+    if col_guess > col_size:
+        print(f'That is not in the boundaries of the ocean! Try a coordinate between 1 and {col_size}')
---
+++ parser.py
@@ -0,0 +1,26 @@
+import argparse
+
+def initialize_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i1", "--rows",
+                        help="Numbers of rows of the board",
+                        type=int,
+                        default=10)
+
+    parser.add_argument("-i2", "--columns",
+                        help="Number of columns of the board",
+                        type=int,
+                        default=10)
+
+    parser.add_argument("-i3", "--ships",
+                        help="The number of ships to play with",
+                        type=int,
+                        default=5)
+
+    parser.add_argument("-i4", "--turns",
+                        help="The number of turns a game should last",
+                        type=int,
+                        default=50)
+
+    return parser.parse_args()
