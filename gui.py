d4003ccdfe10d92f479b3ee69ea0cabb7d7df08a
Massimo-Capurro-Llado <massimocapurrollado@gmail.com>
2022-02-19 12:47:10 +0100

    Now a player can play after placing ships

--- gui.py
+++ gui.py
@@ -1,9 +1,11 @@
+import sys
 import tkinter as tk
 
 import Utils
 import game_board
 import inputs
 import ship_types
+import game
 
 args = inputs.initialize_parser()
 root = tk.Tk()
@@ -11,14 +13,28 @@ root.title('PlayBoard')
 
 
 type_list = Utils.create_ship_type_list(args)
-board, ship_list1 = game_board.create_board(args, type_list)
+board, ships = game_board.create_board(args, type_list)
 
 
-def on_click(row_guess, col_guess, event):
-    if board[row_guess-1][col_guess-1] == 1:
-        color = "red"
-    else:
+def on_click(ship_list, row_guess, col_guess, event):
+    player = 1
+    color = None
+    for i in ship_list:
+        if ship_types.Ship.is_hit(i, row_guess, col_guess):
+            color = "red"
+            if ship_types.Ship.is_sunk(i):
+                if game.is_win(ship_list):
+                    print(f'\nPlayer {player} wins the game')
+                    sys.exit()
+                else:
+                    print('\nHit and sunk a ship, shoot again!')
+                    #Utils.game_variant(ship_list1, ship_list2, args, play_board1, play_board2, player)
+            else:
+                print('\nHit, shoot again!')
+                #Utils.game_variant(ship_list1, ship_list2, args, play_board1, play_board2, player)
+    if color != 'red':
         color = "blue"
+        print('\nMiss, pass the computer to Player2')
     event.widget.config(bg=color)
     board[row_guess-1][col_guess-1] = color
 
@@ -29,7 +45,7 @@ def gui_board(args):
             text = f'({i} - {j})'
             L = tk.Label(root, text=text, bg='grey')
             L.grid(row=i, column=j)
-            L.bind('<Button-1>', lambda e, row_guess=i, col_guess=j: on_click(row_guess, col_guess, e))
+            L.bind('<Button-1>', lambda e, row_guess=i, col_guess=j: on_click(ships, row_guess, col_guess, e))
 
 
 gui_board(args)
