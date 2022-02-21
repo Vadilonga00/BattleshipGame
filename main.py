74b40b78d9aa0f955221af2004650e0081c91c4d
Vadilonga00 <francyvadi@gmail.com>
2022-02-21 14:47:13 +0100

    fix user message

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ args = inputs.initialize_parser()
 
 type_list = Utils.create_ship_type_list(args)
 board_player1, ship_list1 = game_board.create_board(args, type_list)
-print('\n\n\n\nPass now the computer to the other player!')
+print('\n\n\n\n\n\nPass now the computer to the other player!')
 board_player2, ship_list2 = game_board.create_board(args, type_list)
 
 if args.graphics == 1:
     app = gui.GuiApp(args, ship_list1, ship_list2, board_player1, board_player2)
     app.mainloop()
 else:
-    print('\n\n\n\nPass now the computer to player 1 which will start the game!')
     play_board1 = [['-'] * args.columns for x in range(args.rows)]
     play_board2 = [['-'] * args.columns for x in range(args.rows)]
     game.start_game(ship_list1, ship_list2, args, play_board1, play_board2)
