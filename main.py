67391be419e8295fe2b9d2553846ebfc0aaa3843
francescocarpineti091299 <francesco.carpineti@alcampus.it>
2022-02-21 15:35:11 +0100

    User interface fixed

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ args = inputs.initialize_parser()
 
 type_list = Utils.create_ship_type_list(args)
 board_player1, ship_list1 = game_board.create_board(args, type_list)
-print('\n\n\n\n\n\nPass now the computer to the other player!')
+input('\n\nPress enter and pass the computer to the other player:')
+print ("\n"*30)
 board_player2, ship_list2 = game_board.create_board(args, type_list)
+input('\n\nPress enter and pass the computer to the other player to start playing:')
+print ("\n"*30)
 
 if args.graphics == 1:
     app = gui.GuiApp(args, ship_list1, ship_list2, board_player1, board_player2)
